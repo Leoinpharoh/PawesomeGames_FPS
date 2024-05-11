f8c0883313ab5243d99bb29a591b22fee51edda1
Andrew O <141016956+InuXev@users.noreply.github.com>
2024-05-11 16:19:00 -0500

    Working towards UI
    Just a bit of work added scripts for gamemanger and buttonfunctions

---
+++ Unity Project/Pixel Odyssey/Assets/Scripts/GameManager.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using JetBrains.Annotations;
+
+public class GameManager : MonoBehaviour
+{
+    //create the gameManager
+
+    //Fields for menus
+    [SerializeField] GameObject menuActive;
+    [SerializeField] GameObject menuPause;
+    [SerializeField] GameObject menuWin;
+    [SerializeField] GameObject menuLose;
+    [SerializeField] TMP_Text enemyCountText;
+
+    //non serialized
+    //image for hp
+    public Image playerHpBar;
+
+
+    public PlayerManager playerScript;
+
+
+    public GameObject player;
+
+    public bool isPaused;
+
+    int enemyCount;
+
+
+
+    public static GameManager Instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+        //show player location
+        player = GameObject.FindWithTag("Player");
+        //define player script
+        playerScript = player.GetComponent<PlayerManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // to pause
+        if(Input.GetButtonDown("Cancel"))
+        {
+            if(menuActive == null)
+            {
+                statePaused();
+
+                menuActive = menuPause;
+                menuActive.SetActive(isPaused);
+            }
+            else if (menuActive == menuPause)
+            {
+                stateUnPaused();
+            }
+        }
+    }
+
+    public void statePaused()
+    {
+        //
+        isPaused = !isPaused;
+        //keep cursor in the window
+        Cursor.lockState = CursorLockMode.Confined;
+        //hide cursor
+        Cursor.visible = true;
+        //reset time passed to zero
+        Time.timeScale = 0;
+    }
+    public void stateUnPaused()
+    {
+        isPaused = !isPaused;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Confined;
+        //allow time to pass again
+        Time.timeScale = 1;
+        menuActive.SetActive(isPaused);
+        menuActive = null;
+    }
+    public void updateGameGoal(int amount)
+    {
+        enemyCount += amount;
+        enemyCountText.text = amount.ToString("F1");
+        if(enemyCount <= 0)
+        {
+            stateUnPaused();
+            menuActive = menuWin;
+            menuActive.SetActive(isPaused);
+        }
+    }
+
+    public void youLose()
+    {
+        stateUnPaused();
+        menuActive = menuLose;
+        menuActive.SetActive(isPaused);
+    }
+}
