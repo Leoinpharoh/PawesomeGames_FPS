0306199d8091c6291aceb23ddba091229a5be598
Smith-Conner <58701983+Smith-Conner@users.noreply.github.com>
2024-05-12 18:22:29 -0400

    UI

---
+++ Unity Project/Pixel Odyssey/Assets/Scripts/Weapon Script/RaycastGun.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class RaycastGun : MonoBehaviour
+{
+    [SerializeField] int shootDmg;
+    [SerializeField] int shootDist;
+    [SerializeField] float shootSpeed;
+
+    //Used so that ray wont hit player or things like glass
+    [SerializeField] LayerMask shootableLayer;
+    [SerializeField] AudioClip shootAudioSource;
+
+    bool isShooting;
+    [SerializeField] AudioSource Audio;
+
+    //Play Weapon animation
+
+    private void Update()
+    {
+        shoot();
+    }
+
+    void shoot() 
+    { 
+        if (Input.GetButtonDown("Fire1") && !isShooting)
+        {
+            StartCoroutine(shooting());
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            isShooting = false;
+        }
+    }
+
+    IEnumerator shooting()
+    {
+        isShooting = true;
+        Audio.clip = shootAudioSource;
+        Audio.Play();
+
+        RaycastHit hit;
+        if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity, shootableLayer) ) 
+        { 
+            IDamage dmg = hit.collider.GetComponent<IDamage>();
+            if( dmg != null ) { dmg.takeDamage(shootDmg); }
+            Debug.Log(hit.collider.gameObject);
+        }
+        
+
+        yield return new WaitForSeconds(shootSpeed);
+        isShooting = false;
+    }
+}
