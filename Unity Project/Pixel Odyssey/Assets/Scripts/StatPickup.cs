72b669b61ba5fae4d4726d319b0b6a19a98b2069
Smith-Conner <58701983+Smith-Conner@users.noreply.github.com>
2024-05-15 20:19:04 -0400

    Added Cure Pack

--- Unity Project/Pixel Odyssey/Assets/Scripts/StatPickup.cs
+++ Unity Project/Pixel Odyssey/Assets/Scripts/StatPickup.cs
@@ -5,10 +5,9 @@ using UnityEngine;
 
 public class StatPickup : MonoBehaviour
 {
-    [SerializeField] enum PickUpType {Health, Ammo}
+    [SerializeField] enum PickUpType {Health, Ammo, Cure}
     [SerializeField] PickUpType type;
     [SerializeField] int refilAmount;
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
@@ -22,6 +21,13 @@ public class StatPickup : MonoBehaviour
                     break;
                 case PickUpType.Ammo:
                     other.gameObject.GetComponentInChildren<ShootingHandler>().Ammo += refilAmount;
+                    break;
+                    case PickUpType.Cure:
+                    other.gameObject.GetComponent<PlayerManager>().poisoned = false;
+                    other.gameObject.GetComponent<PlayerManager>().burning = false;
+                    other.gameObject.GetComponent<PlayerManager>().freezing = false;
+                    other.gameObject.GetComponent<PlayerManager>().slowed = false;
+                    other.gameObject.GetComponent<PlayerManager>().confused = false;
                     break;
             }
             GameManager.Instance.playerAmmo(other.gameObject.GetComponentInChildren<ShootingHandler>().ammoType.ToString(), other.gameObject.GetComponentInChildren<ShootingHandler>().Ammo);
