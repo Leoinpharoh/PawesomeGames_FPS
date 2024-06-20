596cacb0e8208210ac187eb66a2a091ee9a185a2
Leoinpharoh <zienpharoh@yahoo.com>
2024-06-20 16:36:50 -0400

    Final Boss Animations v1

---
+++ Unity Project/Pixel Odyssey/Assets/Prefabs/Criswell/Enemies/Architect/ArchitectHeadFollow.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArchitectHeadFollow : MonoBehaviour
+{
+    public Transform cameraTransform;
+    private Quaternion initialRotation;
+
+    void Start()
+    {
+        // Store the initial rotation of the object
+        initialRotation = transform.rotation;
+    }
+
+    void Update()
+    {
+        if (cameraTransform != null)
+        {
+            Vector3 directionToCamera = cameraTransform.position - transform.position;
+            Quaternion targetRotation = Quaternion.LookRotation(directionToCamera);
+            targetRotation *= Quaternion.Euler(0, 0, 0); // Apply the corrective rotation
+
+            // Convert target rotation relative to the initial rotation
+            Quaternion relativeRotation = Quaternion.Inverse(initialRotation) * targetRotation;
+
+            // Clamp the Y rotation
+            relativeRotation = ClampRotation(relativeRotation);
+
+            // Convert back to world space rotation
+            targetRotation = initialRotation * relativeRotation;
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 5);
+        }
+    }
+
+    Quaternion ClampRotation(Quaternion q)
+    {
+        q.ToAngleAxis(out float angle, out Vector3 axis);
+        angle = NormalizeAngle(angle);
+
+        // Clamp the angle
+        angle = Mathf.Clamp(angle, -60, 60);
+
+        // Return the new quaternion
+        return Quaternion.AngleAxis(angle, axis);
+    }
+
+    float NormalizeAngle(float angle)
+    {
+        while (angle > 360) angle -= 360;
+        while (angle < 0) angle += 360;
+        if (angle > 180) angle -= 360;
+        return angle;
+    }
+}
