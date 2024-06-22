8e90ea42ce503b4b45766c4379791cefd48bfcd6
Smith-Conner <58701983+Smith-Conner@users.noreply.github.com>
2024-06-22 08:03:21 -0400

    Old Man

--- Unity Project/Pixel Odyssey/Assets/Scripts/Weapon Script/AmmoDisplay.cs
+++ Unity Project/Pixel Odyssey/Assets/Scripts/Weapon Script/AmmoDisplay.cs
@@ -10,6 +10,10 @@ public class AmmoDisplay : MonoBehaviour
 
     int ammo;
 
+    private void Start()
+    {
+        
+    }
     void Update()
     {
         if (weaponStats.Ammo + weaponStats.clip != ammo)
