a954244fb988a9cc414f84e86ae9d415c4d8229e
Andrew O <141016956+InuXev@users.noreply.github.com>
2024-06-24 17:37:21 -0500

    Enemy Running while reloading and small amount of slinding,
    Range issue vs LOS, mixed with some anim needs.

--- Unity Project/Pixel Odyssey/Assets/Scripts/EnemyAI.cs
+++ Unity Project/Pixel Odyssey/Assets/Scripts/EnemyAI.cs
@@ -170,12 +170,10 @@ public class EnemyAI : MonoBehaviour, IDamage
     IEnumerator Reload()
     {
         isReloading = true;
-        agent.isStopped = true;
         anim.SetBool("isReloading", true);
         PlayReloadSound();
         yield return new WaitForSeconds(enemyParams.enemyReloadTime);
         anim.SetBool("isReloading", false);
-        agent.isStopped = false;
         isReloading = false;
     }
 
